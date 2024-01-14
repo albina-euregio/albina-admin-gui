de13c7678f9ea70c3973afe8d257f96985a69905
Simon Legner <Simon.Legner@gmail.com>
2024-01-14 19:13:56 +0100

    observations-api: wikisnow

---
+++ observations-api/fetch_wikisnow.ts
@@ -0,0 +1,18 @@
+import { createConnection, insertObservation } from "./database";
+import { augmentRegion } from "./regions";
+import { ApiWikisnowECT, convertWikisnow } from "../src/app/observations/models/wikisnow.model";
+
+const API = "https://admin.avalanche.report/wikisnow/ect/ect-json.json";
+
+export async function fetchWikiSnow() {
+  console.log("Fetching", API);
+  const json: ApiWikisnowECT = await (await fetch(API)).json();
+
+  const connection = await createConnection();
+  for (let wikisnow of json.data) {
+    let obs = convertWikisnow(wikisnow);
+    obs = augmentRegion(obs);
+    await insertObservation(connection, obs);
+  }
+  connection.destroy();
+}
--- observations-api/index.ts
+++ observations-api/index.ts
@@ -1,5 +1,7 @@
 import { fetchLawisProfiles } from "./fetch_lawis";
 import { fetchLolaKronos } from "./fetch_lola_kronos";
+import { fetchWikiSnow } from "./fetch_wikisnow";
 
 fetchLawisProfiles();
 fetchLolaKronos();
+fetchWikiSnow();
