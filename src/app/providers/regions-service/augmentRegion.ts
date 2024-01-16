34385dcc2e16760faa56d3d415f7a85bf02809b3
Simon Legner <Simon.Legner@gmail.com>
2024-01-16 16:42:41 +0100

    refact: types

--- src/app/providers/regions-service/augmentRegion.ts
+++ src/app/providers/regions-service/augmentRegion.ts
@@ -2,16 +2,22 @@ import whichPolygon from "which-polygon";
 import RegionsEuregio_AT_07 from "@eaws/micro-regions/AT-07_micro-regions.geojson.json";
 import RegionsEuregio_IT_32_BZ from "@eaws/micro-regions/IT-32-BZ_micro-regions.geojson.json";
 import RegionsEuregio_IT_32_TN from "@eaws/micro-regions/IT-32-TN_micro-regions.geojson.json";
-import type { RegionProperties } from "./regions.service";
 import { mergeFeatureCollections } from "./mergeFeatureCollections";
+import { FeatureCollection, MultiPolygon } from "geojson";
+
+type Properties = (typeof RegionsEuregio_IT_32_TN)["features"][number]["properties"];
 
 const query = whichPolygon(
-  mergeFeatureCollections(RegionsEuregio_AT_07, RegionsEuregio_IT_32_BZ, RegionsEuregio_IT_32_TN),
+  mergeFeatureCollections(
+    RegionsEuregio_AT_07 as FeatureCollection<MultiPolygon, Properties>,
+    RegionsEuregio_IT_32_BZ as FeatureCollection<MultiPolygon, Properties>,
+    RegionsEuregio_IT_32_TN as FeatureCollection<MultiPolygon, Properties>,
+  ),
 );
 
 export function getRegionForLatLng(observation: { latitude?: number; longitude?: number }): string {
   if (observation.latitude && observation.longitude) {
-    const region = query([observation.longitude, observation.latitude], false) as RegionProperties;
+    const region = query([observation.longitude, observation.latitude], false) as Properties;
     return region?.id;
   }
   return undefined;
