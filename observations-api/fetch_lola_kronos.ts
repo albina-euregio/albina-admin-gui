fd4cd756d27a6f5d602f36880a9c5f2c9c0dff86
Simon Legner <Simon.Legner@gmail.com>
2024-01-26 20:22:43 +0100

    refact: process.env.LOLA_KRONOS_API_TOKEN

--- observations-api/fetch_lola_kronos.ts
+++ observations-api/fetch_lola_kronos.ts
@@ -1,15 +1,16 @@
 import dayjs from "dayjs";
 import { LolaKronosApi, convertLoLaKronos } from "../src/app/observations/models/lola-kronos.model";
 
-const API = "https://admin.avalanche.report/lola-kronos/dataexport/dataFromToken/";
+const API = "https://www.lola-kronos.info/api/dataexport/dataFromToken/";
 const WEB = "https://www.lola-kronos.info/";
 
 export async function* fetchLolaKronos() {
   const startDate = formatDate(dayjs().millisecond(0).subtract(1, "week"));
   const endDate = formatDate(dayjs().millisecond(0));
   const url = `${API}${startDate}/${endDate}`;
+  const headers = { Authorization: process.env.ALBINA_LOLA_KRONOS_API_TOKEN };
   console.log("Fetching", url);
-  const json: LolaKronosApi = await (await fetch(url)).json();
+  const json: LolaKronosApi = await (await fetch(url, { headers })).json();
 
   for (let obs of convertLoLaKronos(json, WEB)) {
     yield obs;
