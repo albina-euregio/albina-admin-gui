d53bf6677a54ae9e5c4a2f22bb585491288ffeba
Simon Legner <Simon.Legner@gmail.com>
2024-04-25 15:40:00 +0200

    chore(filterFeature): const TODAY for performance

--- src/app/providers/regions-service/filterFeature.ts
+++ src/app/providers/regions-service/filterFeature.ts
@@ -1,7 +1,6 @@
-export function filterFeature(
-  feature: GeoJSON.Feature,
-  today = new Date().toISOString().slice(0, "2006-01-02".length),
-): boolean {
+const TODAY = new Date().toISOString().slice(0, "2006-01-02".length);
+
+export function filterFeature(feature: GeoJSON.Feature, today = TODAY): boolean {
   const properties = feature.properties;
   return (
     (!properties.start_date || properties.start_date <= today) && (!properties.end_date || properties.end_date > today)
