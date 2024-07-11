5f521249bd490707e4965f4659cb100d4f96d685
Simon Legner <Simon.Legner@gmail.com>
2024-07-11 03:58:26 +0200

    refact: use danger-rating.component

--- src/app/bulletins/danger-rating.component.ts
+++ src/app/bulletins/danger-rating.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input } from "@angular/core";
-import { BulletinDaytimeDescriptionModel } from "../models/bulletin-daytime-description.model";
-import { SettingsService } from "../providers/settings-service/settings.service";
 import * as Enums from "../enums/enums";
 
 @Component({
@@ -9,24 +7,5 @@ import * as Enums from "../enums/enums";
 })
 export class DangerRatingComponent {
   dangerRating = Enums.DangerRating;
-
-  @Input() bulletinDaytimeDescription: BulletinDaytimeDescriptionModel;
-  @Input() below: boolean;
-  @Input() disabled: boolean;
-
-  constructor(public settingsService: SettingsService) {}
-
-  isDangerRating(dangerRating: Enums.DangerRating) {
-    return this.below
-      ? this.bulletinDaytimeDescription.dangerRatingBelow === dangerRating
-      : this.bulletinDaytimeDescription.dangerRatingAbove === dangerRating;
-  }
-
-  selectDangerRating(dangerRating: Enums.DangerRating) {
-    if (this.below) {
-      this.bulletinDaytimeDescription.setDangerRatingBelow(dangerRating);
-    } else {
-      this.bulletinDaytimeDescription.setDangerRatingAbove(dangerRating);
-    }
-  }
+  @Input() value: Enums.DangerRating;
 }
