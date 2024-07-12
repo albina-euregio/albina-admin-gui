8fb926537a62c375f47bf26d923d091f860ba600
Simon Legner <Simon.Legner@gmail.com>
2024-07-12 21:43:43 +0200

    refact(danger-rating): signals

--- src/app/bulletins/danger-rating.component.ts
+++ src/app/bulletins/danger-rating.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, input } from "@angular/core";
 import * as Enums from "../enums/enums";
 
 @Component({
   selector: "app-danger-rating",
   templateUrl: "danger-rating.component.html",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DangerRatingComponent {
   dangerRating = Enums.DangerRating;
-  @Input() value: Enums.DangerRating;
+  value = input<Enums.DangerRating>();
 }
