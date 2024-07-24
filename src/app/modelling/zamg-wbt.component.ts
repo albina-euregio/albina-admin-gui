445c6422ce97c7f8074ca071ab1a450a786a5485
Simon Legner <Simon.Legner@gmail.com>
2024-07-24 07:49:17 +0200

    feat: integrate ZAMG Wetterbox as link

--- src/app/modelling/zamg-wbt.component.ts
+++ src/app/modelling/zamg-wbt.component.ts
@@ -4,11 +4,13 @@ import { Component } from "@angular/core";
   selector: "app-zamg-wbt",
   standalone: true,
   imports: [],
-  template: `<iframe
-    src="https://portal.tirol.gv.at/at.ac.zamg.wbt-p/"
-    style="width: 100%; height: 90vh"
-    referrerpolicy="no-referrer"
-    allow="fullscreen"
-  ></iframe>`,
+  template: `
+    <a
+      href="https://portal.tirol.gv.at/at.ac.zamg.wbt-p/"
+      class="btn btn-success btn-lg position-absolute top-50 start-50 translate-middle"
+    >
+      Wetterinformationsportal © ZAMG
+    </a>
+  `,
 })
 export class ZamgWbtComponent {}
