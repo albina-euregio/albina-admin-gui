8ccf270c76fbfda6dafc85061a244ad6668424b3
Simon Legner <Simon.Legner@gmail.com>
2024-08-14 09:46:58 +0200

    refact(app.component): simplify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, Inject } from "@angular/core";
-import { TranslateService } from "@ngx-translate/core";
-import { SettingsService } from "./providers/settings-service/settings.service";
-import { DOCUMENT } from "@angular/common";
 import { environment } from "../environments/environment";
 
 @Component({
@@ -10,11 +7,7 @@ import { environment } from "../environments/environment";
   template: "<router-outlet></router-outlet>",
 })
 export class AppComponent {
-  constructor(
-    settingsService: SettingsService,
-    translate: TranslateService,
-    @Inject(DOCUMENT) _document: HTMLDocument,
-  ) {
-    _document.getElementById("appFavicon").setAttribute("href", `${environment.faviconPath}`);
+  constructor() {
+    document.getElementById("appFavicon").setAttribute("href", `${environment.faviconPath}`);
   }
 }
