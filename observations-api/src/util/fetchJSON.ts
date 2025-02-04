d53778e1336ddb7060873bfaa26e0b5f93860a91
Simon Legner <Simon.Legner@gmail.com>
2025-02-04 21:22:23 +0100

    fix(observations-api): fetchJSON from file:// for node:22

--- observations-api/src/util/fetchJSON.ts
+++ observations-api/src/util/fetchJSON.ts
@@ -1,4 +1,10 @@
+import { readFile } from "fs/promises";
+
 export async function fetchJSON(url: RequestInfo, options?: RequestInit): Promise<any> {
+  if (typeof url === "string" && url.startsWith("file://")) {
+    const json = await readFile(url.slice("file://".length), { encoding: "utf-8" });
+    return JSON.parse(json);
+  }
   console.log("Fetching", url);
   const res = await fetch(url, {
     cache: "no-cache",
