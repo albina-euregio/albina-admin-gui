def02414ca1d6a2fe1e7a106938dd943f8ce2323
Simon Legner <Simon.Legner@gmail.com>
2025-02-24 19:21:54 +0100

    refact(observations-api): move fetchSnowLineCalculations

--- observations-api/src/fetch/weather-stations.ts
+++ observations-api/src/fetch/weather-stations.ts
@@ -1,7 +1,6 @@
 import { average, max, median, min, sum } from "simple-statistics";
 import { type GenericObservation, ObservationSource, ObservationType } from "../generic-observation";
 import { fetchJSON, fetchText } from "../util/fetchJSON";
-import { fetchSnowLineCalculations } from "./observations/snow_line";
 import groupBy from "lodash/groupBy";
 
 export async function getAwsWeatherStations(
@@ -242,4 +241,51 @@ function buildStatistics(parameter: ParameterType, unit: string, values: number[
     sum: values.length ? sum(values) : NaN,
     delta: values.length ? values.at(-1) - values.at(0) : NaN,
   };
+}
+
+/**
+ * Calculated snow fall levels from weather stations
+ * https://gitlab.com/lwd.met/lwd-internal-projects/snow-fall-level-calculator
+ */
+export async function* fetchSnowLineCalculations(
+  startDate: Date,
+  endDate: Date,
+): AsyncGenerator<GenericObservation, void, unknown> {
+  const API = "https://static.avalanche.report/snow-fall-level-calculator/geojson/{{date}}.geojson";
+  const WEB = "https://static.avalanche.report/snow-fall-level-calculator/Plots/weekly/{{date}}/{{plot}}";
+  interface Properties {
+    station_number: string;
+    station_name: string;
+    subregion: string;
+    region: string;
+    extended_region: string;
+    snowfall_limit: number;
+    plot_name: string;
+  }
+  while (+endDate > +startDate) {
+    const date = endDate.toISOString().slice(0, "2006-01-02".length);
+    const url = API.replace("{{date}}", date);
+    try {
+      const json: GeoJSON.FeatureCollection<GeoJSON.Point, Properties> = await fetchJSON(url);
+      for (const feature of json.features) {
+        yield {
+          $type: ObservationType.DrySnowfallLevel,
+          $source: ObservationSource.AvalancheWarningService,
+          $id: date + "-" + feature.properties.station_number,
+          $data: feature.properties,
+          eventDate: endDate,
+          reportDate: endDate,
+          locationName: feature.properties.station_name,
+          $externalImgs: [WEB.replace("{{date}}", date).replace("{{plot}}", feature.properties.plot_name)],
+          latitude: feature.geometry.coordinates[1],
+          longitude: feature.geometry.coordinates[0],
+          elevation: feature.properties.snowfall_limit,
+        } satisfies GenericObservation;
+      }
+    } catch (err) {
+      console.log(`Failed to fetch ${url}`, err);
+    }
+    endDate = new Date(endDate);
+    endDate.setDate(endDate.getDate() - 1);
+  }
 }
