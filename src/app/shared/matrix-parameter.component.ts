d59c70a9297741fb56cbdd8b97acc5c4520dffbb
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2025-05-23 09:12:06 +0200

    chore(matrix-paramater): restrict snowpack stability and frequency according to avalanche type

--- src/app/shared/matrix-parameter.component.ts
+++ src/app/shared/matrix-parameter.component.ts
@@ -26,6 +26,7 @@ export class MatrixParameterComponent implements OnInit {
   readonly count = input<number>(undefined);
   readonly afternoon = input<boolean>(undefined);
   readonly changeMatrixEvent = output();
+  readonly avalancheType = input<Enums.AvalancheType>(undefined);
 
   dangerRating = Enums.DangerRating;
   dangerRatingEnabled = false;
@@ -35,113 +36,285 @@ export class MatrixParameterComponent implements OnInit {
     return this.count() + (this.afternoon() ? "_pm_" : "_am_") + key;
   }
 
-  snowpackStabilityOptions: SliderOptions = {
-    floor: 25,
-    ceil: 100,
-    ticks: [25, 37, 50, 62, 75, 87, 100],
-    getLegend: (value: number): string => {
-      switch (value) {
-        case 37:
-          return this.translateService.instant("snowpackStability.fair");
-        case 62:
-          return this.translateService.instant("snowpackStability.poor");
-        case 87:
-          return this.translateService.instant("snowpackStability.very_poor");
-        default:
-          return "";
-      }
-    },
-    getSelectionBarColor: (value: number): string => {
-      if (value < 0) {
-        return "lightgrey";
-      }
-      if (value < 25) {
-        return "green";
-      }
-      if (value < 50) {
-        return "yellow";
-      }
-      if (value < 75) {
-        return "orange";
-      }
-      if (value >= 75) {
-        return "red";
-      }
-      return "lightgrey";
-    },
-    onValueChange: (value: number) => {
-      this.matrixInformation().snowpackStabilityValue = value;
-      switch (true) {
-        case value < 25:
-          this.setSnowpackStability(Enums.SnowpackStability.good);
-          break;
-        case value < 50:
-          this.setSnowpackStability(Enums.SnowpackStability.fair);
-          break;
-        case value < 75:
-          this.setSnowpackStability(Enums.SnowpackStability.poor);
-          break;
-        default:
-          this.setSnowpackStability(Enums.SnowpackStability.very_poor);
-          break;
-      }
-      this.changeMatrixEvent.emit();
-    },
-  };
+  getSnowpackStabilityOptions(avalancheType: Enums.AvalancheType): SliderOptions {
+    switch (avalancheType) {
+      case Enums.AvalancheType.glide:
+        return {
+          floor: 75,
+          ceil: 100,
+          ticks: [75, 87, 100],
+          getLegend: (value: number): string => {
+            switch (value) {
+              case 87:
+                return this.translateService.instant("snowpackStability.very_poor");
+              default:
+                return "";
+            }
+          },
+          getSelectionBarColor: (value: number): string => {
+            if (value < 0) {
+              return "lightgrey";
+            }
+            if (value < 25) {
+              return "green";
+            }
+            if (value < 50) {
+              return "yellow";
+            }
+            if (value < 75) {
+              return "orange";
+            }
+            if (value >= 75) {
+              return "red";
+            }
+            return "lightgrey";
+          },
+          onValueChange: (value: number) => {
+            this.matrixInformation().snowpackStabilityValue = value;
+            switch (true) {
+              case value < 25:
+                this.setSnowpackStability(Enums.SnowpackStability.good);
+                break;
+              case value < 50:
+                this.setSnowpackStability(Enums.SnowpackStability.fair);
+                break;
+              case value < 75:
+                this.setSnowpackStability(Enums.SnowpackStability.poor);
+                break;
+              default:
+                this.setSnowpackStability(Enums.SnowpackStability.very_poor);
+                break;
+            }
+            this.changeMatrixEvent.emit();
+          },
+        };
+      case Enums.AvalancheType.loose:
+        return {
+          floor: 50,
+          ceil: 100,
+          ticks: [50, 62, 75, 87, 100],
+          getLegend: (value: number): string => {
+            switch (value) {
+              case 62:
+                return this.translateService.instant("snowpackStability.poor");
+              case 87:
+                return this.translateService.instant("snowpackStability.very_poor");
+              default:
+                return "";
+            }
+          },
+          getSelectionBarColor: (value: number): string => {
+            if (value < 0) {
+              return "lightgrey";
+            }
+            if (value < 25) {
+              return "green";
+            }
+            if (value < 50) {
+              return "yellow";
+            }
+            if (value < 75) {
+              return "orange";
+            }
+            if (value >= 75) {
+              return "red";
+            }
+            return "lightgrey";
+          },
+          onValueChange: (value: number) => {
+            this.matrixInformation().snowpackStabilityValue = value;
+            switch (true) {
+              case value < 25:
+                this.setSnowpackStability(Enums.SnowpackStability.good);
+                break;
+              case value < 50:
+                this.setSnowpackStability(Enums.SnowpackStability.fair);
+                break;
+              case value < 75:
+                this.setSnowpackStability(Enums.SnowpackStability.poor);
+                break;
+              default:
+                this.setSnowpackStability(Enums.SnowpackStability.very_poor);
+                break;
+            }
+            this.changeMatrixEvent.emit();
+          },
+        };
+      default:
+        return {
+          floor: 25,
+          ceil: 100,
+          ticks: [25, 37, 50, 62, 75, 87, 100],
+          getLegend: (value: number): string => {
+            switch (value) {
+              case 37:
+                return this.translateService.instant("snowpackStability.fair");
+              case 62:
+                return this.translateService.instant("snowpackStability.poor");
+              case 87:
+                return this.translateService.instant("snowpackStability.very_poor");
+              default:
+                return "";
+            }
+          },
+          getSelectionBarColor: (value: number): string => {
+            if (value < 0) {
+              return "lightgrey";
+            }
+            if (value < 25) {
+              return "green";
+            }
+            if (value < 50) {
+              return "yellow";
+            }
+            if (value < 75) {
+              return "orange";
+            }
+            if (value >= 75) {
+              return "red";
+            }
+            return "lightgrey";
+          },
+          onValueChange: (value: number) => {
+            this.matrixInformation().snowpackStabilityValue = value;
+            switch (true) {
+              case value < 25:
+                this.setSnowpackStability(Enums.SnowpackStability.good);
+                break;
+              case value < 50:
+                this.setSnowpackStability(Enums.SnowpackStability.fair);
+                break;
+              case value < 75:
+                this.setSnowpackStability(Enums.SnowpackStability.poor);
+                break;
+              default:
+                this.setSnowpackStability(Enums.SnowpackStability.very_poor);
+                break;
+            }
+            this.changeMatrixEvent.emit();
+          },
+        };
+    }
+  }
 
-  frequencyOptions: SliderOptions = {
-    floor: 25,
-    ceil: 100,
-    ticks: [25, 37, 50, 62, 75, 87, 100],
-    getLegend: (value: number): string => {
-      switch (value) {
-        case 37:
-          return this.translateService.instant("frequency.few");
-        case 62:
-          return this.translateService.instant("frequency.some");
-        case 87:
-          return this.translateService.instant("frequency.many");
-        default:
-          return "";
-      }
-    },
-    getSelectionBarColor: (value: number): string => {
-      if (value < 0) {
-        return "lightgrey";
-      }
-      if (value < 25) {
-        return "green";
-      }
-      if (value < 50) {
-        return "yellow";
-      }
-      if (value < 75) {
-        return "orange";
-      }
-      if (value >= 75) {
-        return "red";
-      }
-      return "lightgrey";
-    },
-    onValueChange: (value: number) => {
-      this.matrixInformation().frequencyValue = value;
-      switch (true) {
-        case value < 25:
-          this.setFrequency(Enums.Frequency.none);
-          break;
-        case value < 50:
-          this.setFrequency(Enums.Frequency.few);
-          break;
-        case value < 75:
-          this.setFrequency(Enums.Frequency.some);
-          break;
-        default:
-          this.setFrequency(Enums.Frequency.many);
-          break;
-      }
-      this.changeMatrixEvent.emit();
-    },
-  };
+  getFrequencyOptions(avalancheType: Enums.AvalancheType): SliderOptions {
+    switch (avalancheType) {
+      case Enums.AvalancheType.loose:
+      case Enums.AvalancheType.glide:
+        return {
+          floor: 25,
+          ceil: 75,
+          ticks: [25, 37, 50, 62, 75],
+          getLegend: (value: number): string => {
+            switch (value) {
+              case 37:
+                return this.translateService.instant("frequency.few");
+              case 62:
+                return this.translateService.instant("frequency.some");
+              default:
+                return "";
+            }
+          },
+          getSelectionBarColor: (value: number): string => {
+            if (value < 0) {
+              return "lightgrey";
+            }
+            if (value < 25) {
+              return "green";
+            }
+            if (value < 50) {
+              return "yellow";
+            }
+            if (value >= 50) {
+              return "orange";
+            }
+            return "lightgrey";
+          },
+          onValueChange: (value: number) => {
+            this.matrixInformation().frequencyValue = value;
+            switch (true) {
+              case value < 25:
+                this.setFrequency(Enums.Frequency.none);
+                break;
+              case value < 50:
+                this.setFrequency(Enums.Frequency.few);
+                break;
+              case value < 75:
+                this.setFrequency(Enums.Frequency.some);
+                break;
+              default:
+                if (this.avalancheType() === Enums.AvalancheType.slab || !this.avalancheType()) {
+                  this.setFrequency(Enums.Frequency.many);
+                } else {
+                  this.matrixInformation().frequencyValue = 75;
+                  this.setFrequency(Enums.Frequency.some);
+                }
+                break;
+            }
+            this.changeMatrixEvent.emit();
+          },
+        };
+      default:
+        return {
+          floor: 25,
+          ceil: 100,
+          ticks: [25, 37, 50, 62, 75, 87, 100],
+          getLegend: (value: number): string => {
+            switch (value) {
+              case 37:
+                return this.translateService.instant("frequency.few");
+              case 62:
+                return this.translateService.instant("frequency.some");
+              case 87:
+                return this.translateService.instant("frequency.many");
+              default:
+                return "";
+            }
+          },
+          getSelectionBarColor: (value: number): string => {
+            if (value < 0) {
+              return "lightgrey";
+            }
+            if (value < 25) {
+              return "green";
+            }
+            if (value < 50) {
+              return "yellow";
+            }
+            if (value < 75) {
+              return "orange";
+            }
+            if (value >= 75 && this.avalancheType() === Enums.AvalancheType.slab) {
+              return "red";
+            }
+            return "lightgrey";
+          },
+          onValueChange: (value: number) => {
+            this.matrixInformation().frequencyValue = value;
+            switch (true) {
+              case value < 25:
+                this.setFrequency(Enums.Frequency.none);
+                break;
+              case value < 50:
+                this.setFrequency(Enums.Frequency.few);
+                break;
+              case value < 75:
+                this.setFrequency(Enums.Frequency.some);
+                break;
+              default:
+                if (this.avalancheType() === Enums.AvalancheType.slab || !this.avalancheType()) {
+                  this.setFrequency(Enums.Frequency.many);
+                } else {
+                  this.matrixInformation().frequencyValue = 75;
+                  this.setFrequency(Enums.Frequency.some);
+                }
+                break;
+            }
+            this.changeMatrixEvent.emit();
+          },
+        };
+    }
+  }
 
   avalancheSizeOptions: SliderOptions = {
     floor: 0,
