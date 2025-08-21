19dcb3365cc5b3e075fd4d9423028dbe366baf30
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2025-08-21 08:49:02 +0200

    refactor(regions-service): format date

--- src/app/providers/regions-service/mergeFeatureCollections.ts
+++ src/app/providers/regions-service/mergeFeatureCollections.ts
@@ -1,9 +1,10 @@
 import { filterFeature } from "./filterFeature";
+import { formatDate } from "@angular/common";
 
 export function mergeFeatureCollections<G extends GeoJSON.Geometry, P>(
   ...collections: GeoJSON.FeatureCollection<G, P>[]
 ): GeoJSON.FeatureCollection<G, P> {
-  const today = new Date().toISOString().slice(0, "2006-01-02".length);
+  const today = formatDate(Date.now(), "yyyy-MM-dd", "en-US");
   return {
     type: "FeatureCollection",
     features: []
