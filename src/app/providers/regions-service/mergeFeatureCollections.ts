c60bd08922d9004a25efdedaf5b5bb775f6ff43f
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2025-08-21 08:37:02 +0200

    fix(micro-regions): use current date for micro region filtering

--- src/app/providers/regions-service/mergeFeatureCollections.ts
+++ src/app/providers/regions-service/mergeFeatureCollections.ts
@@ -3,7 +3,7 @@ import { filterFeature } from "./filterFeature";
 export function mergeFeatureCollections<G extends GeoJSON.Geometry, P>(
   ...collections: GeoJSON.FeatureCollection<G, P>[]
 ): GeoJSON.FeatureCollection<G, P> {
-  const today = "2022-12-01";
+  const today = new Date().toISOString().slice(0, "2006-01-02".length);
   return {
     type: "FeatureCollection",
     features: []
