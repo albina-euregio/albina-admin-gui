0ad64aba6385c76872835907a108dba069c0dd1d
Christina Kirk <christina.kirk@tbbm.at>
2025-10-14 09:49:32 +0200

    Playwright: more bulletin tests

--- tests/bulletins.spec.ts
+++ tests/bulletins.spec.ts
@@ -48,7 +48,32 @@ test("View bulletin", async ({ page }) => {
   });
 
   await test.step("Foreign regions are visible", async () => {
-    // TODO
+    await expect(page.locator(".region-thumb").filter({ hasText: "Prealps + 3" })).toMatchAriaSnapshot(`
+      - button "Prealps + 3"
+      - button "Marta Pendesini"
+      - img
+      - text: / \\d+ m/
+      - button ""
+    `);
+  });
+
+  await test.step("External regions are visible", async () => {
+    await page.locator("header").filter({ hasText: "GeoSphere (12)" }).getByRole("button").click();
+    await expect(page.locator(".region-thumb").filter({ hasText: "Werdenfels Alps + 4" })).toMatchAriaSnapshot(`
+      - button "Werdenfels Alps + 4"
+      - button "Christoph Hummel"
+      - img
+      - text: / \\d+ m/
+      - button ""
+    `);
+    await page.locator("header").filter({ hasText: "SLF (7)" }).getByRole("button").click();
+    await expect(page.locator(".region-thumb").filter({ hasText: "Münstertal + 45" })).toMatchAriaSnapshot(`
+      - button /Münstertal \\+ \\d+/
+      - button
+      - text: 
+      - button "Incomplete"
+      - button ""
+    `);
   });
 });
 
@@ -100,27 +125,78 @@ test("Edit bulletin", async ({ page }) => {
     await expect(regionProblem.getByText("Treeline").first()).toBeVisible();
   });
   await test.step("undo/redo", async () => {
-    // TODO
-  });
-  await test.step("textcat", async () => {
-    // TODO
-  });
-  await test.step("undo/redo", async () => {
-    // TODO
+    // undoes the whole problem, since playwright clicks the problem parameters so fast
+    await page.getByTitle("Undo").click();
+    await expect(page.getByRole("tab", { name: /SLAB.*Treeline/ })).toBeHidden();
+    await page.getByTitle("Redo").click();
+    await expect(page.getByRole("tab", { name: /SLAB.*Treeline/ })).toBeVisible();
   });
   await test.step("daytime dependency", async () => {
-    // TODO
+    await page.getByTitle("Daytime dependency").click();
+    await expect(page.getByRole("tab", { name: /SLAB.*Treeline/ })).toBeVisible();
+    await page.getByRole("button", { name: /Avalanche problems Afternoon/ }).click();
+    await expect(page.getByRole("tab", { name: /SLAB.*Treeline/ }).nth(1)).toBeVisible();
+    await expect(page.locator("#afternoonMap")).toBeVisible();
+    // TODO export JSON and compare
+    await page.getByTitle("Daytime dependency").click();
+    await page.getByRole("button", { name: "Later" }).click();
+    await expect(page.locator("#afternoonMap")).toBeHidden();
+    await expect(page.getByText("Forenoon")).toBeHidden();
   });
   await test.step("edit microregions", async () => {
-    // TODO
+    await page.getByTitle("Edit micro regions").click();
+    await expect(page.getByText("Select regions on the map.")).toBeVisible();
+    await page.locator("path.leaflet-interactive").nth(93).click({ force: true });
+    await page.getByRole("button", { name: "Save changes" }).click();
+    await expect(regionProblem).toContainText("Brandenberg Alps + 1");
+  });
+  await test.step("delete problem", async () => {
+    await page.getByRole("button", { name: " Avalanche problems " }).click();
+    await page
+      .getByRole("tab", { name: /SLAB.*Treeline/ })
+      .getByTitle("Delete")
+      .click();
+    await expect(page.getByRole("tab", { name: /SLAB.*Treeline/ })).toBeHidden();
   });
   await test.step("delete region", async () => {
-    // TODO
+    await page.getByTitle("Delete region [del]").click();
+    await expect(page.getByText("Do you want to delete this region?")).toBeVisible();
+    await page.getByRole("button", { name: "Yes" }).click();
+    await expect(regionProblem).not.toContainText("Brandenberg");
   });
 });
 
 test("Copy foreign region", async ({ page }) => {
-  // TODO
+  const testDate = new Date("2025-01-10");
+  await page.clock.setFixedTime(testDate);
+  await page.reload();
+  await changeRegion(page, "Tyrol");
+
+  await page.getByRole("row", { name: "Saturday, January 11, 2025" }).getByRole("button").nth(1).click();
+  await page.locator(".region-thumb").filter({ hasText: "Gröden" }).getByTitle("Copy region").click();
+  await page.getByRole("row", { name: "Saturday, January 11, 2025 " }).getByRole("button").click();
+  await expect(page.getByText("Copy warning region to another bulletin")).toBeHidden();
+  await expect(page.getByText("Select regions on the map")).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Saturday, January 11," })).toBeVisible();
+  await page.locator("path.leaflet-interactive").nth(93).click({ force: true });
+  await page.locator("path.leaflet-interactive").nth(100).click({ force: true });
+  const responsePromise = page.waitForResponse(
+    (response) =>
+      response.url().match(/\/api\/bulletins/) && response.status() === 200 && response.request().method() === "PUT",
+  );
+  await page.getByRole("button", { name: "Create region" }).click();
+  await responsePromise;
+  await page.reload();
+  await expect(page.locator(".region-thumb").filter({ hasText: "Brandenberg Alps + 1" })).toMatchAriaSnapshot(`
+    - button "Brandenberg Alps + 1"
+    - button "Playwright"
+    - img
+    - img
+    - button ""
+    - button ""
+  `);
+  await page.locator(".region-thumb").filter({ hasText: "Brandenberg Alps + 1" }).getByTitle("Delete region").click();
+  await page.getByRole("button", { name: "Yes" }).click();
 });
 
 test("Bulletin synchronization", async ({ page }) => {
@@ -142,12 +218,26 @@ test("Load bulletin from the day before", async ({ page }) => {
     await test.step("Delete all warning regions", async () => {
       await page.getByTitle("[b]").click();
       await page.getByRole("button", { name: "Delete all warning regions" }).click();
+      const responsePromise = page.waitForResponse(
+        (response) =>
+          response.url().match(/\/api\/bulletins/) &&
+          response.status() === 200 &&
+          response.request().method() === "DELETE",
+      );
       await page.getByRole("dialog").getByRole("button", { name: "Yes" }).click();
+      await responsePromise;
     });
     await test.step("Load from the day before", async () => {
       await page.getByTitle("[b]").click();
       await page.getByRole("button", { name: "Load bulletin from the day before" }).click();
+      const responsePromise = page.waitForResponse(
+        (response) =>
+          response.url().match(/\/api\/bulletins/) &&
+          response.status() === 200 &&
+          response.request().method() === "POST",
+      );
       await page.getByRole("dialog").getByRole("button", { name: "Yes" }).click();
+      await responsePromise;
       await expect(page.locator(".badge").first()).toContainText("draft");
       await expect(page.getByRole("heading", { name: "Tyrol (4)" })).toBeVisible();
       await expect(page.locator(".region-thumb")).toContainText([
@@ -161,7 +251,12 @@ test("Load bulletin from the day before", async ({ page }) => {
   await test.step("Load into existing bulletin", async () => {
     await page.getByTitle("[b]").click();
     await page.getByRole("button", { name: "Load bulletin from the day before" }).click();
+    const responsePromise = page.waitForResponse(
+      (response) =>
+        response.url().match(/\/api\/bulletins/) && response.status() === 200 && response.request().method() === "POST",
+    );
     await page.getByRole("dialog").getByRole("button", { name: "Yes" }).click();
+    await responsePromise;
     await expect(page.locator(".badge").first()).toContainText("draft");
     await expect(page.getByRole("heading", { name: "Tyrol (4)" })).toBeVisible();
     await expect(page.locator(".region-thumb")).toContainText([
