96f97a41ee2d75e0304beffa29aec1f217ceab7b
fynn_at_jackcachpot_de <fynn@jackcachpot.de>
2026-01-08 08:13:22 +0100

    added linea export route

---
+++ src/app/modelling/lineaexport.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "lineaexport",
+  standalone: true,
+  imports: [],
+  template: `
+    <a
+      href="https://portal.tirol.gv.at/at.ac.zamg.wbt-p/"
+      class="btn btn-success btn-lg position-absolute top-50 start-50 translate-middle"
+      target="_blank"
+    >
+      Wetterinformationsportal © ZAMG
+    </a>
+  `,
+})
+export class LineaExportComponent {}
