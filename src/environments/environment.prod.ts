bb22344e9117125a333696456cb15f20e11c2fce
Simon Legner <Simon.Legner@gmail.com>
2026-03-05 17:26:02 +0100

    chore(awsome): use models.awsome.alpsolut.eu for prod+beta
    Fixes #451.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
   initialUrl: "",
   apiBaseUrl: "https://admin.avalanche.report/albina/api/",
   textcatUrl: "https://admin.avalanche.report/textcat-ng/",
-  awsomeConfigUrl: "https://models.avalanche.report/dcfg/awsome.json",
+  awsomeConfigUrl: "https://models.awsome.alpsolut.eu/dcfg/awsome.json",
   headerBgColor: "#ffffff",
   faviconPath: "assets/img/admin-favicon.ico",
   logoPath: "assets/img/admin-logo.svg",
