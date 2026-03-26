9bd43128646c7b2c822157b3709e0e964b1757ec
fynn_at_jackcachpot_de <fynn@jackcachpot.de>
2026-03-26 13:15:25 +0100

    feat: add yearly stats

--- src/app/graphics/graphics.component.ts
+++ src/app/graphics/graphics.component.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from "@angular/common";
 import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from "@angular/core";
-import { TranslateModule } from "@ngx-translate/core";
-import { LineaExportComponent } from "./lineaexport.component";
-import { AwsstatsComponent } from "./awsstats.component";
-import { TabsModule } from "ngx-bootstrap/tabs";
-import { AuthenticationService } from "app/providers/authentication-service/authentication.service";
 import { ActivatedRoute, Router } from "@angular/router";
+import { TranslateModule } from "@ngx-translate/core";
+import { AuthenticationService } from "app/providers/authentication-service/authentication.service";
+import { TabsModule } from "ngx-bootstrap/tabs";
+
+import { AwsstatsComponent } from "./awsstats.component";
+import { LineaExportComponent } from "./lineaexport.component";
+import { YearlystatsComponent } from "./yearlystats.component";
 
 @Component({
   selector: "app-graphics",
   standalone: true,
-  imports: [CommonModule, TranslateModule, LineaExportComponent, AwsstatsComponent, TabsModule],
+  imports: [CommonModule, TranslateModule, LineaExportComponent, AwsstatsComponent, YearlystatsComponent, TabsModule],
   templateUrl: "./graphics.component.html",
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
@@ -22,20 +24,24 @@ export class GraphicsComponent implements OnInit {
   activeTabIndex = 0;
 
   ngOnInit() {
-    const initialTab = this.activatedRoute.snapshot.params["tab"] as "lineaexport" | "awsstats" | undefined;
-    if (initialTab === "lineaexport" || initialTab === "awsstats") {
+    const initialTab = this.activatedRoute.snapshot.params["tab"] as
+      | "lineaexport"
+      | "awsstats"
+      | "yearlystats"
+      | undefined;
+    if (initialTab === "lineaexport" || initialTab === "awsstats" || initialTab === "yearlystats") {
       this.setActiveTab(initialTab);
     }
     this.activatedRoute.params.subscribe((params) => {
-      const tab = params["tab"] as "lineaexport" | "awsstats" | undefined;
-      if (tab === "lineaexport" || tab === "awsstats") {
+      const tab = params["tab"] as "lineaexport" | "awsstats" | "yearlystats" | undefined;
+      if (tab === "lineaexport" || tab === "awsstats" || tab === "yearlystats") {
         this.setActiveTab(tab);
       }
     });
   }
 
-  protected setActiveTab(tab: "lineaexport" | "awsstats") {
-    this.activeTabIndex = tab === "awsstats" ? 1 : 0;
+  protected setActiveTab(tab: "lineaexport" | "awsstats" | "yearlystats") {
+    this.activeTabIndex = tab === "yearlystats" ? 2 : tab === "awsstats" ? 1 : 0;
   }
 
   protected showLineaExport() {
@@ -48,5 +54,11 @@ export class GraphicsComponent implements OnInit {
     if (this.activeTabIndex === 1) return;
     this.activeTabIndex = 1;
     this.router.navigate(["awsstats"], { relativeTo: this.activatedRoute.parent });
+  }
+
+  protected showYearlyStats() {
+    if (this.activeTabIndex === 2) return;
+    this.activeTabIndex = 2;
+    this.router.navigate(["yearlystats"], { relativeTo: this.activatedRoute.parent });
   }
 }
