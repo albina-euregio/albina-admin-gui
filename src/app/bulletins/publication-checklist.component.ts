b09a88cb065e7b146d7d5bdded3cdb268748c540
Christina Kirk <christina.kirk@tbbm.at>
2026-04-02 12:51:40 +0200

    feat(publication-checklist): translations

--- src/app/bulletins/publication-checklist.component.ts
+++ src/app/bulletins/publication-checklist.component.ts
@@ -1,11 +1,13 @@
 import { Component, computed, inject } from "@angular/core";
 import { toSignal } from "@angular/core/rxjs-interop";
 import { ActivatedRoute } from "@angular/router";
+import { TranslateModule } from "@ngx-translate/core";
 import { map } from "rxjs";
 
 @Component({
   templateUrl: "publication-checklist.component.html",
   standalone: true,
+  imports: [TranslateModule],
 })
 export class PublicationChecklistComponent {
   private route = inject(ActivatedRoute);
