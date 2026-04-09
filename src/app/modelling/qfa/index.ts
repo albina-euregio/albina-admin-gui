195354cbb6fa49292de1614685dac71f2d0d13c5
Simon Legner <Simon.Legner@gmail.com>
2026-04-09 15:59:45 +0200

    refact(qfa): inline class GetFilenamesService

--- src/app/modelling/qfa/index.ts
+++ src/app/modelling/qfa/index.ts
@@ -1,4 +1,3 @@
 export * from "./dust.service";
-export * from "./filenames.service";
 export * from "./param.service";
 export * from "./qfa.service";
--- src/app/modelling/qfa/qfa.service.ts
+++ src/app/modelling/qfa/qfa.service.ts
@@ -1,12 +1,38 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
 import type { LatLngExpression } from "leaflet";
+import { firstValueFrom, map } from "rxjs";
+import { z } from "zod";
 
 import { GetDustParamService } from "./dust.service";
-import { GetFilenamesService, QfaItem } from "./filenames.service";
 import { ParamService } from "./param.service";
 import { QfaFile } from "./qfa-file.model";
 import * as types from "./qfa-types";
+
+export const CaddyListingItemSchema = z.object({
+  name: z.string(),
+  size: z.number(),
+  url: z.string(),
+  mod_time: z.coerce.date(),
+  mode: z.number(),
+  is_dir: z.boolean(),
+  is_symlink: z.boolean(),
+});
+
+export type CaddyListingItem = z.infer<typeof CaddyListingItemSchema>;
+
+const QfaItemSchema = z.object({
+  date: z.string(),
+  hours: z.string(),
+  minutes: z.string(),
+  startDay: z.string(),
+  endDay: z.string(),
+  city: z.string(),
+  qfa: z.string(),
+  filename: z.string(),
+});
+
+export type QfaItem = z.infer<typeof QfaItemSchema>;
 
 export interface QfaFilename {
   filename: string;
@@ -24,7 +50,6 @@ type City = "bozen" | "innsbruck" | "lienz";
 
 @Injectable()
 export class QfaService {
-  filenamesService = inject(GetFilenamesService);
   dustParamService = inject(GetDustParamService);
   paramService = inject(ParamService);
   private http = inject(HttpClient);
@@ -56,12 +81,59 @@ export class QfaService {
     this.dustParams = await this.dustParamService.parseDustParams();
   }
 
+  private getHTMLResponse() {
+    // Caddy serves directory index as JSON for Accept=application/json
+    // https://github.com/caddyserver/caddy/blob/e8ad9b32c9730ddb162b6fb1443fc0b36fcef7dc/modules/caddyhttp/fileserver/browse.go#L105-L109
+    return this.http.get(this.baseUrl).pipe(map((r) => CaddyListingItemSchema.array().parse(r)));
+  }
+
+  async getFilenames(baseUrl: string, city: string) {
+    this.baseUrl = baseUrl;
+    const response = await firstValueFrom(this.getHTMLResponse());
+    const files = response.reverse();
+    // console.log(files);
+    const filteredFiles = files.filter((file) => file.name.includes(city));
+    return filteredFiles;
+  }
+
+  parseFilename(filename: string): QfaItem {
+    const parts = filename.split("_");
+    return QfaItemSchema.parse({
+      date: parts[0],
+      hours: parts[1].substring(0, 2),
+      minutes: parts[1].substring(2, 4),
+      startDay: parts[4].substring(0, 2),
+      endDay: parts[4].substring(2, 4),
+      city: parts[3],
+      qfa: parts[2],
+      filename: filename,
+    });
+  }
+
+  stringifyFile(file: QfaItem): string {
+    return `${file.date}_${file.hours}${file.minutes}_${file.qfa}_${file.city}_${file.startDay}${file.endDay}.txt`;
+  }
+
+  changeDay(file: QfaItem, startDay: string, endDay: string): QfaItem {
+    file.startDay = startDay;
+    file.endDay = endDay;
+    file.filename = this.stringifyFile(file);
+    return file;
+  }
+
+  getCityName(files: QfaItem[]): string | void {
+    if (files.length) {
+      const name = files[0].city.charAt(0).toUpperCase() + files[0].city.slice(1);
+      return name;
+    }
+  }
+
   async getFiles() {
     for (const city of this.cities) {
-      const filenames = await this.filenamesService.getFilenames(this.baseUrl, city);
+      const filenames = await this.getFilenames(this.baseUrl, city);
       const parsedFiles = [];
       for (const file of filenames) {
-        const parsedFile = this.filenamesService.parseFilename(file.name);
+        const parsedFile = this.parseFilename(file.name);
         parsedFiles.push(parsedFile);
       }
       this.files[city] = parsedFiles.filter((el) => el.startDay === "00");
