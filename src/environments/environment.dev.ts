e4ae016bed052a8147976d5681ce034222c1acb6
Christina Kirk <christina.kirk@tbbm.at>
2026-04-21 16:32:48 +0200

    chore: update dev environment URLs
    see albina-euregio/albina-infrastructure#100

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -6,8 +6,8 @@ import type { Environment } from "./environment-type";
 
 export const environment: Environment = {
   initialUrl: "",
-  apiBaseUrl: "https://admin.avalanche.report/albina_dev/api/",
-  textcatUrl: "https://admin.avalanche.report/textcat-ng-dev/",
+  apiBaseUrl: "https://dev.avalanche.report/api/",
+  textcatUrl: "https://dev.avalanche.report/textcat-ng/",
   awsomeConfigUrl: "https://models.avalanche.report/dcfg/awsome.json",
   headerBgColor: "#f4ea12",
   faviconPath: "assets/img/admin-favicon-dev.ico",
