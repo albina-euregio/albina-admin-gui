e297965643dd18b602ccf1f2adae81dc14a254a3
Christina Kirk <christina.kirk@tbbm.at>
2026-05-04 15:21:03 +0200

    feat(statistics): show alert when download failed
    related to albina-euregio/albina-server#389

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,9 +1,11 @@
+import { HttpErrorResponse } from "@angular/common/http";
 import { Component, inject } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { TranslateModule, TranslateService } from "@ngx-translate/core";
 import { saveAs } from "file-saver";
 import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 import { TabsModule } from "ngx-bootstrap/tabs";
+import { TimeoutError, firstValueFrom } from "rxjs";
 
 import { ConstantsService } from "../providers/constants-service/constants.service";
 import { StatisticsService } from "../providers/statistics-service/statistics.service";
@@ -20,64 +22,84 @@ export class StatisticsComponent {
   constantsService = inject(ConstantsService);
 
   public loadingStatistics = false;
+  public statisticsError = "";
   public bsRangeValueBulletin: Date[];
   public duplicates: boolean;
   public extended: boolean;
 
   public bsRangeValueDangerSource: Date[];
 
-  getBulletinStatistics(event) {
+  async getBulletinStatistics(event: Event) {
     event.stopPropagation();
-    if (this.bsRangeValueBulletin) {
-      this.loadingStatistics = true;
-      //document.getElementById("overlay").style.display = "block";
-      this.statisticsService
-        .getBulletinStatisticsCsv(
+
+    if (!this.bsRangeValueBulletin) {
+      return;
+    }
+
+    this.loadingStatistics = true;
+    this.statisticsError = "";
+
+    try {
+      const blob = await firstValueFrom(
+        this.statisticsService.getBulletinStatisticsCsv(
           this.bsRangeValueBulletin[0],
           this.bsRangeValueBulletin[1],
           this.translateService.getCurrentLang(),
           this.extended,
           this.duplicates,
-        )
-        .subscribe((blob) => {
-          this.loadingStatistics = false;
-          //document.getElementById("overlay").style.display = "none";
-          const startDate = this.constantsService.getISODateString(this.bsRangeValueBulletin[0]);
-          const endDate = this.constantsService.getISODateString(this.bsRangeValueBulletin[1]);
-          let filename = "statistic_" + startDate + "_" + endDate;
-          if (this.extended || this.duplicates) {
-            filename = filename + "_";
-            if (this.duplicates) {
-              filename = filename + "d";
-            }
-            if (this.extended) {
-              filename = filename + "e";
-            }
-          }
-          filename = filename + "_" + this.translateService.getCurrentLang() + ".csv";
-          saveAs(blob, filename);
-          console.log("Bulletin statistics loaded.");
-        });
+        ),
+      );
+      const startDate = this.constantsService.getISODateString(this.bsRangeValueBulletin[0]);
+      const endDate = this.constantsService.getISODateString(this.bsRangeValueBulletin[1]);
+      let filename = "statistic_" + startDate + "_" + endDate;
+      if (this.extended || this.duplicates) {
+        filename = filename + "_";
+        if (this.duplicates) {
+          filename = filename + "d";
+        }
+        if (this.extended) {
+          filename = filename + "e";
+        }
+      }
+      filename = filename + "_" + this.translateService.getCurrentLang() + ".csv";
+      saveAs(blob, filename);
+      console.log("Bulletin statistics loaded.");
+    } catch (error) {
+      this.statisticsError = this.translateService.instant("statistics.error.downloadFailed");
+      console.error("Bulletin statistics could not be downloaded.", error);
+    } finally {
+      this.loadingStatistics = false;
     }
   }
 
-  getDangerSourceStatistics(event) {
+  async getDangerSourceStatistics(event: Event) {
     event.stopPropagation();
-    if (this.bsRangeValueDangerSource) {
-      this.loadingStatistics = true;
-      //document.getElementById("overlay").style.display = "block";
-      this.statisticsService
-        .getDangerSourceStatisticsCsv(this.bsRangeValueDangerSource[0], this.bsRangeValueDangerSource[1])
-        .subscribe((blob) => {
-          this.loadingStatistics = false;
-          //document.getElementById("overlay").style.display = "none";
-          const startDate = this.constantsService.getISODateString(this.bsRangeValueDangerSource[0]);
-          const endDate = this.constantsService.getISODateString(this.bsRangeValueDangerSource[1]);
-          let filename = "danger_source_statistic_" + startDate + "_" + endDate;
-          filename = filename + ".csv";
-          saveAs(blob, filename);
-          console.log("Danger source statistics loaded.");
-        });
+
+    if (!this.bsRangeValueDangerSource) {
+      return;
+    }
+
+    this.loadingStatistics = true;
+    this.statisticsError = "";
+
+    try {
+      const blob = await firstValueFrom(
+        this.statisticsService.getDangerSourceStatisticsCsv(
+          this.bsRangeValueDangerSource[0],
+          this.bsRangeValueDangerSource[1],
+        ),
+      );
+      const startDate = this.constantsService.getISODateString(this.bsRangeValueDangerSource[0]);
+      const endDate = this.constantsService.getISODateString(this.bsRangeValueDangerSource[1]);
+      let filename = "danger_source_statistic_" + startDate + "_" + endDate;
+      filename = filename + ".csv";
+      saveAs(blob, filename);
+      console.log("Danger source statistics loaded.");
+    } catch (error) {
+      this.statisticsError = this.translateService.instant("statistics.error.downloadFailed");
+      console.error("Danger source statistics could not be downloaded.", error);
+    } finally {
+      this.loadingStatistics = false;
     }
   }
 }
