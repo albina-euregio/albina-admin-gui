9f29dc1198b8a192890adcec92a22985b5fdde9d
Simon Legner <Simon.Legner@gmail.com>
2026-05-27 15:46:34 +0200

    chore(zod): zod feedback for toggle-btn-group

--- src/app/shared/zod-css-class.ts
+++ src/app/shared/zod-css-class.ts
@@ -1,9 +1,9 @@
 import { z } from "zod/v4";
 
-export function zodCssClass<T>(zodType: z.ZodType<T>, value: T) {
+export function zodCssClass<T>(zodType: z.ZodType<T>, value: T, mainClass = "form-control") {
   const result = zodType.safeParse(value);
   return {
-    "form-control": true,
+    [mainClass]: true,
     "is-valid": (Array.isArray(value) ? value.length : value) && result.success,
     "is-invalid": result.error,
   };
