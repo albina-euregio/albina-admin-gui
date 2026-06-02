d13a12179c4718b4a2422d139e1f39ff178f5360
Simon Legner <Simon.Legner@gmail.com>
2026-06-02 08:04:58 +0200

    chore(zod): widgetRegistry.valueI18n

--- src/app/shared/zod-schema-form.widget-registry.ts
+++ src/app/shared/zod-schema-form.widget-registry.ts
@@ -1,11 +1,8 @@
 import { z } from "zod/v4";
 
-export const WidgetSchema = z
-  .object({
-    unit: z.string(),
-    showIf: z.string().array(),
-    widget: z.enum(["slider", "radio", "checkbox"]),
-  })
-  .partial();
-type Widget = z.infer<typeof WidgetSchema>;
-export const widgetRegistry = z.registry<Widget>();
+export const widgetRegistry = z.registry<{
+  unit?: string;
+  valueI18n?: `${string}#${string}`;
+  showIf?: string[];
+  widget?: "slider" | "radio" | "checkbox";
+}>();
