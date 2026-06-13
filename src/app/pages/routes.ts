dd16084019ec81ef3f8752235e6246cce19249f2
Simon Legner <Simon.Legner@gmail.com>
2026-06-13 21:48:04 +0200

    refact(pages): unused 404, 500 component

--- src/app/pages/routes.ts
+++ src/app/pages/routes.ts
@@ -1,33 +1,11 @@
 import { Routes } from "@angular/router";
 
-import { AuthGuard } from "../guards/auth.guard";
-import { P404Component } from "./404.component";
-import { P500Component } from "./500.component";
 import { LoginComponent } from "./login.component";
 
 export default [
   {
     path: "",
-    data: {
-      title: "Example Pages",
-    },
     children: [
-      {
-        path: "404",
-        component: P404Component,
-        canActivate: [AuthGuard],
-        data: {
-          title: "Page 404",
-        },
-      },
-      {
-        path: "500",
-        component: P500Component,
-        canActivate: [AuthGuard],
-        data: {
-          title: "Page 500",
-        },
-      },
       {
         path: "login",
         component: LoginComponent,
