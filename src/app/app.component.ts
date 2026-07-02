ce8ef6b8176568b46601d112a83cba01071db17c
Christina Kirk <christina.kirk@tbbm.at>
2026-07-02 14:45:43 +0200

    refact(i18n): extract incident-report strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,13 @@ import de from "../assets/i18n/de.json";
 import en from "../assets/i18n/en.json";
 import es from "../assets/i18n/es.json";
 import fr from "../assets/i18n/fr.json";
+import incidentReportCa from "../assets/i18n/incident-report/ca.json";
+import incidentReportDe from "../assets/i18n/incident-report/de.json";
+import incidentReportEn from "../assets/i18n/incident-report/en.json";
+import incidentReportEs from "../assets/i18n/incident-report/es.json";
+import incidentReportFr from "../assets/i18n/incident-report/fr.json";
+import incidentReportIt from "../assets/i18n/incident-report/it.json";
+import incidentReportOc from "../assets/i18n/incident-report/oc.json";
 import it from "../assets/i18n/it.json";
 import oc from "../assets/i18n/oc.json";
 import { environment } from "../environments/environment";
@@ -27,13 +34,13 @@ export class AppComponent {
   constructor() {
     // lang
     this.translateService.addLangs(["de", "it", "en", "fr", "es", "ca", "oc"]);
-    this.translateService.setTranslation("de", de);
-    this.translateService.setTranslation("it", it);
-    this.translateService.setTranslation("en", en);
-    this.translateService.setTranslation("fr", fr);
-    this.translateService.setTranslation("es", es);
-    this.translateService.setTranslation("ca", ca);
-    this.translateService.setTranslation("oc", oc);
+    this.translateService.setTranslation("de", { ...de, ...incidentReportDe });
+    this.translateService.setTranslation("it", { ...it, ...incidentReportIt });
+    this.translateService.setTranslation("en", { ...en, ...incidentReportEn });
+    this.translateService.setTranslation("fr", { ...fr, ...incidentReportFr });
+    this.translateService.setTranslation("es", { ...es, ...incidentReportEs });
+    this.translateService.setTranslation("ca", { ...ca, ...incidentReportCa });
+    this.translateService.setTranslation("oc", { ...oc, ...incidentReportOc });
     // this language will be used as a fallback when a translation isn't found in the current language
     this.translateService.setFallbackLang("en");
     // the lang to use, if the lang isn't available, it will use the current loader to get them
