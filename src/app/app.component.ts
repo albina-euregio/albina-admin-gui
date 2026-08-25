5da0e0edc2645edfce14a8a7ee23084671842f92
Simon Legner <Simon.Legner@gmail.com>
2026-08-25 21:12:38 +0200

    refact(i18n): remove strings duplicated by app/i18n/caaml bundle from main i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component, inject, ChangeDetectionStrategy } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { TranslateService } from "@ngx-translate/core";
 
+import caamlCa from "../../app/i18n/caaml/ca.json";
+import caamlDe from "../../app/i18n/caaml/de.json";
+import caamlEn from "../../app/i18n/caaml/en.json";
+import caamlEs from "../../app/i18n/caaml/es.json";
+import caamlFr from "../../app/i18n/caaml/fr.json";
+import caamlIt from "../../app/i18n/caaml/it.json";
+import caamlOc from "../../app/i18n/caaml/oc.json";
 import ca from "../assets/i18n/ca.json";
 import de from "../assets/i18n/de.json";
 import en from "../assets/i18n/en.json";
@@ -33,25 +40,35 @@ export class AppComponent {
 
   constructor() {
     // lang
-    // The incident-report resource is layered on top of the main resource with
-    // shouldMerge=true so ngx-translate deep-merges them: shared categories
+    // The incident-report and caaml resources are layered on top of the main resource
+    // with shouldMerge=true so ngx-translate deep-merges them: shared categories
     // (e.g. avalancheType) keep the main resource's extra nested keys like
     // .title/.label/.tooltip instead of being clobbered by a shallow spread.
+    // The caaml resource uses flat dot-keys (e.g. "dangerRating.low"), which only
+    // resolve when the main resource has no nested object under that first segment
+    // (e.g. "aspect", "tendency" stay defined in the main resource on purpose).
     this.translateService.addLangs(["de", "it", "en", "fr", "es", "ca", "oc"]);
     this.translateService.setTranslation("de", de);
     this.translateService.setTranslation("de", incidentReportDe, true);
+    this.translateService.setTranslation("de", caamlDe, true);
     this.translateService.setTranslation("it", it);
     this.translateService.setTranslation("it", incidentReportIt, true);
+    this.translateService.setTranslation("it", caamlIt, true);
     this.translateService.setTranslation("en", en);
     this.translateService.setTranslation("en", incidentReportEn, true);
+    this.translateService.setTranslation("en", caamlEn, true);
     this.translateService.setTranslation("fr", fr);
     this.translateService.setTranslation("fr", incidentReportFr, true);
+    this.translateService.setTranslation("fr", caamlFr, true);
     this.translateService.setTranslation("es", es);
     this.translateService.setTranslation("es", incidentReportEs, true);
+    this.translateService.setTranslation("es", caamlEs, true);
     this.translateService.setTranslation("ca", ca);
     this.translateService.setTranslation("ca", incidentReportCa, true);
+    this.translateService.setTranslation("ca", caamlCa, true);
     this.translateService.setTranslation("oc", oc);
     this.translateService.setTranslation("oc", incidentReportOc, true);
+    this.translateService.setTranslation("oc", caamlOc, true);
     // this language will be used as a fallback when a translation isn't found in the current language
     this.translateService.setFallbackLang("en");
     // the lang to use, if the lang isn't available, it will use the current loader to get them
